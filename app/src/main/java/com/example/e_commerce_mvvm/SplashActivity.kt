da08a263925c3113f691c8a9772c89784468b5e5
Victor Adrián Dominguez Garcia <gtmcpinwii@gmail.com>
2025-05-09 17:05:49 -0600

    [ADD] Splash Activity

---
+++ app/src/main/java/com/example/e_commerce_mvvm/SplashActivity.kt
@@ -0,0 +1,26 @@
+package com.example.e_commerce_mvvm
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.e_commerce_mvvm.databinding.ActivitySplashBinding
+
+class SplashActivity : AppCompatActivity() {
+    lateinit var binding: ActivitySplashBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        listeners()
+    }
+
+    private fun listeners() {
+        binding.startBtn.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+    }
+}
